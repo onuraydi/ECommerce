058c8a8e01075291d203e86ba9e9a085f11060a2
onuraydi <onuraydi@outlook.com>
2024-09-24 14:04:28 +0300

    Sending Mail

---
+++ Frontends/Ecommerce.WebUI/Controllers/MailController.cs
@@ -0,0 +1,41 @@
+﻿using Ecommerce.WebUI.Models;
+using MailKit.Net.Smtp;
+using Microsoft.AspNetCore.Mvc;
+using MimeKit;
+
+namespace Ecommerce.WebUI.Controllers
+{
+    public class MailController : Controller
+    {
+        [HttpGet]
+        public IActionResult SendMail()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SendMail(MailRequest mailRequest)
+        {
+            MimeMessage mimeMessage = new MimeMessage();
+            
+            MailboxAddress mailboxAddressFrom = new MailboxAddress("ecommerce", "yazilimdeneme1934@gmail.com");
+            mimeMessage.From.Add(mailboxAddressFrom);
+            
+            MailboxAddress mailboxAddressTo = new MailboxAddress("user",mailRequest.RecieverMail);
+            mimeMessage.To.Add(mailboxAddressTo);
+
+            var bodyBuilder = new BodyBuilder();
+            bodyBuilder.TextBody = mailRequest.Content;
+
+            mimeMessage.Body = bodyBuilder.ToMessageBody();
+            mimeMessage.Subject = mailRequest.Subject;
+
+            SmtpClient client = new SmtpClient();
+            client.Connect("smtp.gmail.com", 587, false);
+            client.Authenticate("yazilimdeneme1934@gmail.com", "yzszxppscwzwtrxv");
+            client.Send(mimeMessage);
+            client.Disconnect(true);
+            return View();
+        }
+    }
+}
---
+++ Frontends/Ecommerce.WebUI/Models/MailRequest.cs
@@ -0,0 +1,12 @@
+﻿
+namespace Ecommerce.WebUI.Models
+{
+    public class MailRequest
+    {
+        public string RecieverMail { get; set; }
+        public string Subject { get; set; }
+        public string Content { get; set; }
+
+
+    }
+}
